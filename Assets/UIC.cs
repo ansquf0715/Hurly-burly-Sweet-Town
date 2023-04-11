2b24b1ce400250a79b21ee5ed6760f73ddd16002
[ansquf0715] <ansquf0715@naver.com>
2023-04-11 23:50:11 +0900

    오류수정

--- Assets/UIC.cs
+++ Assets/UIC.cs
@@ -26,13 +26,18 @@ public class UI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        menuPage.SetActive(false);
+        if (menuPage != null)
+            menuPage.SetActive(false);
 
-        audioManager = GameObject.Find("AudioManager");
-        stage1AudioSource = audioManager.GetComponent<AudioSource>();
+        if (GameObject.Find("AudioManager") != null)
+        {
+            audioManager = GameObject.Find("AudioManager");
+            stage1AudioSource = audioManager.GetComponent<AudioSource>();
+        }
 
-        //musicSlider.value = musicSlider.maxValue;
-        musicSlider.value = stage1AudioSource.volume;
+
+        musicSlider.value = musicSlider.maxValue;
+        //musicSlider.value = stage1AudioSource.volume;
     }
 
     // Update is called once per frame
@@ -54,17 +59,52 @@ public class UI : MonoBehaviour
 
     public void clickGoToMain()
     {
+        //SceneManager.LoadScene("Title");
+        PlayerPrefs.SetInt("heartNum", 3);
+        PlayerPrefs.Save();
+
+        setGameNum();
+
+        if (audioManager != null)
+            GameObject.Find("AudioManager").GetComponent<AudioManagerScript>().replay();
         SceneManager.LoadScene("Title");
+        Time.timeScale = 1;
+
     }
 
     public void clickRestart()
     {
-        SceneManager.LoadScene("Stage1Cooking");
+        //SceneManager.LoadScene("Stage1Cooking");
+
+        PlayerPrefs.SetInt("heartNum", 3);
+        PlayerPrefs.Save();
+
+        setGameNum();
+        if (audioManager != null)
+            GameObject.Find("AudioManager").GetComponent<AudioManagerScript>().replay();
+        SceneManager.LoadScene("Stage1");
+        Time.timeScale = 1;
+
     }
 
     public void clickQuitGame()
     {
-        Application.Quit();
+        //Application.Quit();
+        PlayerPrefs.SetInt("heartNum", 3);
+        PlayerPrefs.Save();
+
+        setGameNum();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; //play모드를 false로.
+#elif UNITY_WEBPLAYER
+        Application.OpenURL("http://google.com"); //구글웹으로 전환
+#else
+        Application.Quit(); //어플리케이션 종료
+#endif
+
+        Time.timeScale = 1;
+
     }
 
     public void ToggleMusic()
